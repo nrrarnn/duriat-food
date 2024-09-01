7ebe9525ffb073caa63725e3f2a0d9b6a5db7dd6
Nur Arini <nurarini0302@gmail.com>
2024-09-01 16:03:36 +0700

    [UPDATE] tambah pagination

--- src/components/CustomerReviews.tsx
+++ src/components/CustomerReviews.tsx
@@ -10,32 +10,49 @@ interface Review {
 
 const CustomerReviews: React.FC = () => {
   const [reviews, setReviews] = useState<Review[]>([]);
+  const [visibleReviews, setVisibleReviews] = useState<number>(5);
 
   const fetchReviews = async () => {
     try {
-      const response = await axios.get("https://66d3be525b34bcb9ab3cee6e.mockapi.io/ulasan/ulasan-customer")
-      setReviews(response.data)
-    } catch(error) {
-      console.log(error)
-    }    
+      const response = await axios.get("https://66d3be525b34bcb9ab3cee6e.mockapi.io/ulasan/ulasan-customer");
+      setReviews(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
-    fetchReviews(); 
-  }, [reviews]);
+    fetchReviews();
+  }, []);
+
+  const showMoreReviews = () => {
+    setVisibleReviews((prevVisible) => prevVisible + 5);
+  };
 
   return (
     <div className="p-4">
       <h2 className="text-center text-xl font-bold">Ulasan Pelanggan</h2>
       <div className="space-y-4 mt-4">
-        {reviews.map((review) => (
+        {reviews.slice(0, visibleReviews).map((review) => (
           <div key={review.id} className="p-4 border rounded shadow">
             <h3 className="font-semibold text-lg">{review.name}</h3>
             <p className="text-gray-600">{review.ulasan}</p>
-            <p className="text-sm text-gray-500">Dikirim pada {new Date(review.createdAt).toLocaleDateString()}</p>
+            <p className="text-sm text-gray-500">
+              Dikirim pada {new Date(review.createdAt).toLocaleDateString()}
+            </p>
           </div>
         ))}
       </div>
+      {visibleReviews < reviews.length && (
+        <div className="text-center mt-4">
+          <button
+            className="button bg-orange-400 rounded"
+            onClick={showMoreReviews}
+          >
+            Show More
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/FloatingButton.tsx
+++ src/components/FloatingButton.tsx
@@ -5,9 +5,10 @@ const FloatingButton: React.FC = () => {
   return (
     <a
       href="https://wa.me/62895380084949"
-      className="fixed bottom-6 right-6 bg-orange-500 text-white text-lg font-semibold py-3 px-6 rounded-full shadow-lg transition-transform transform hover:scale-105"
+      className="fixed flex justify-center bottom-6 right-6 bg-orange-500 text-white text-lg font-semibold py-3 px-6 rounded-full shadow-lg transition-transform transform hover:scale-105"
     >
-      <FaWhatsapp/>
+      <FaWhatsapp className='flex mt-1.5 mr-1'/> 
+      <p>Order now</p>
     </a>
   );
 };
